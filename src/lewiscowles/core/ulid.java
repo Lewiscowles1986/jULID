c65ba693b29ee0ebcc7859a1d1352eece9011282
Lewis Cowles <lewiscowles@me.com>
2016-08-02 19:55:20 +0100

    updated

--- src/lewiscowles/core/ulid.java
+++ src/lewiscowles/core/ulid.java
@@ -6,11 +6,11 @@ import static java.lang.Math.random;
 
 
 public final class ulid {
-    public String[] ENCODING = { "0","1","2","3","4","5","6","7","8","9",
-                                 "A","B","C","D","E","F","G","H","J","K",
-                                 "M","N","P","Q","R","S","T","V","W","X",
-                                 "Y","Z"};
-    public static long ENCODING_LENGTH = 32;
+    public final String[] ENCODING = {  "0","1","2","3","4","5","6","7","8","9",
+                                        "A","B","C","D","E","F","G","H","J","K",
+                                        "M","N","P","Q","R","S","T","V","W","X",
+                                        "Y","Z"};
+    public final long ENCODING_LENGTH = 32;
 
     public String get()
     {
