606832492db5dffd918700e2254271df144707ca
Lewis Cowles <lewiscowles@me.com>
2020-07-27 04:00:27 +0100

    Move to HamCrest 2 & jUnit 5

--- test/lewiscowles/core/UlidTest.java
+++ test/lewiscowles/core/UlidTest.java
@@ -1,13 +1,11 @@
 package lewiscowles.core;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
-
-import org.junit.Before;
-import org.junit.Test;
+import static org.hamcrest.Matchers.equalTo;
 
 public class UlidTest {
 
@@ -16,7 +14,7 @@ public class UlidTest {
     protected TimeSourceInterface mTS;
     protected final long TIME = 1469918176385l;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         mTS = new SystemTimeSource();
         mRG = new SecureRandomGenerator();
@@ -26,7 +24,7 @@ public class UlidTest {
     @Test
     public void newUlidShouldReturnCorrectLength() {
         String hash = mUlid.get();
-        assertEquals( hash.length(), 26 );
+        assertThat(hash.length(), equalTo(26));
     }
 
     @Test
@@ -43,29 +41,29 @@ public class UlidTest {
     public void encodeTimeShouldReturnExpectedEncodedResult()
     {
         String hash = mUlid.encodeTime(TIME, 10);
-        assertEquals("01ARYZ6S41", hash);
+        assertThat(hash, equalTo("01ARYZ6S41"));
     }
 
     @Test
     public void encodeTimeShouldChangeLengthProperly()
     {
         String hash = mUlid.encodeTime(TIME,12);
-        assertEquals("0001ARYZ6S41", hash);
+        assertThat(hash, equalTo("0001ARYZ6S41"));
     }
 
     @Test
     public void encodeTimeShouldTruncateTimeIfNotLongEnough()
     {
         String hash = mUlid.encodeTime(TIME, 8);
-        assertEquals("ARYZ6S41", hash);
+        assertThat(hash, equalTo("ARYZ6S41"));
     }
 
     @Test
     public void encodeRandomShouldReturnCorrectLength()
     {
-        long length = 12;
+        int length = 12;
         String hash = mUlid.encodeRandom(length);
-        assertEquals(length, hash.length());
+        assertThat(hash.length(), equalTo(length));
     }
 
 }
